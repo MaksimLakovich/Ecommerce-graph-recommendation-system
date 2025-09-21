1477584d5e7146d1e695660c6ba267c2d75a764d
Maksim Lakovich <maks.lakovich@gmail.com>
2025-09-21 18:16:37 +0300

    Создал админки для моделей UserInteraction и UserPreference

--- preferences/admin.py
+++ preferences/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from preferences.models import UserInteraction, UserPreference
+
+
+@admin.register(UserInteraction)
+class AdminUserInteraction(admin.ModelAdmin):
+    """Настройка отображения данных модели UserInteraction в админке."""
+    list_display = (
+        "id",
+        "user_id",
+        "product_id",
+        "aisle_id",
+        "interaction_type",
+        "source",
+        "weight",
+    )
+    list_filter = ("interaction_type", "source",)
+    search_fields = (
+        "user_id__email",
+        "interaction_type",
+        "source",
+    )
+
+
+@admin.register(UserPreference)
+class AdminUserPreference(admin.ModelAdmin):
+    """Настройка отображения данных модели UserPreference в админке."""
+    list_display = (
+        "id",
+        "user_id",
+        "product_id",
+        "aisle_id",
+    )
+    list_filter = ("user_id",)
+    search_fields = (
+        "user_id__email",
+    )
