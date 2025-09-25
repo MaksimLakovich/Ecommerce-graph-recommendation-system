3a7af01deb0b622a89689cb26ffbb94dafa795ac
Maksim Lakovich <maks.lakovich@gmail.com>
2025-09-25 18:36:47 +0300

    Доработал сброс кэша

--- preferences/signals.py
+++ preferences/signals.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -7,5 +8,12 @@ from recommender.services import reset_recommendations_cache
 
 @receiver(post_save, sender=UserInteraction)
 def reset_cache_on_user_interaction(sender, instance, **kwargs):
-    """Сбрасывает кэш рекомендаций после любого изменения UserInteraction."""
-    reset_recommendations_cache(instance.user_id_id)
+    """Сбрасывает кэш рекомендаций после изменения UserInteraction.
+    Используем transaction.on_commit, чтобы дождаться фиксации всех изменений в БД, а не сбрасывать
+    после каждого post_save()."""
+    user_id = instance.user_id_id
+
+    def _reset_cache():
+        reset_recommendations_cache(user_id)
+
+    transaction.on_commit(_reset_cache)
